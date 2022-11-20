8573e9cd678f519653caec3267ed2f13847e39eb
shehabhoss123 <83165114+shehabhoss123@users.noreply.github.com>
2022-11-20 23:30:11 +0200

    Add files via upload

---
+++ Location-Reminder-main/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,93 @@
+package com.udacity.project4.authentication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
+import com.udacity.project4.locationreminders.RemindersActivity
+
+/**
+ * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
+ * signed in users to the RemindersActivity.
+ */
+class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_authentication
+        )
+
+        binding.lifecycleOwner = this
+
+        binding.signInButton.setOnClickListener {
+            launchSignInFlow()
+        }
+
+    }
+
+    private fun launchSignInFlow() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+            // This is where you can provide more ways for users to register and
+            // sign in.
+        )
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
+                startReminderActivity()
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                Toast.makeText(this, "Sign in unsuccessful", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startReminderActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+}
---
+++ Location-Reminder-main/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -0,0 +1,56 @@
+package com.udacity.project4.locationreminders.data
+
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import net.bytebuddy.implementation.bytecode.Throw
+
+//Use FakeDataSource that acts as a test double to the LocalDataSource
+class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+
+
+    private var shouldReturnError = false
+
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if (shouldReturnError) {
+        return Result.Error(
+            "Error getting reminders"
+        )
+    }
+        remindersList?.let { return Result.Success(it) }
+        return Result.Error("Reminders not found")
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+       remindersList?.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        val reminder = remindersList?.find { reminderDTO ->
+            reminderDTO.id == id
+        }
+
+       return when {
+            shouldReturnError -> {
+                Result.Error("Reminder not found!")
+            }
+
+           reminder != null -> {
+               Result.Success(reminder)
+           }
+           else -> {
+               Result.Error("Reminder not found!")
+           }
+        }
+    }
+
+    override suspend fun deleteAllReminders() {
+        remindersList?.clear()
+    }
+
+}
